49f1eceef8b3713882ad9865c187125598ca992f
Peter Schafhalter <pschafhalter@berkeley.edu>
2015-12-21 23:57:05 -0800

    Added live checks for writing chorales

--- utils.py
+++ utils.py
@@ -1,5 +1,33 @@
-from music21 import note, stream, chord, roman
+from music21 import note, stream, chord, roman, pitch
 from voice_leading import error_checks
+
+
+def live_check_chorale(chorale=None):
+    """Checks chorale from user input chord by chord.
+    """
+    print("Input notes as instructed or enter 0 to exit")
+    if chorale:
+        parts = [chorale.getElementById(p) for p in ["soprano", "alto", "tenor", "bass"]]
+    else:
+        parts = []
+        for p in ["soprano", "alto", "tenor", "bass"]:
+            part = stream.Part()
+            part.id = p
+            parts.append(part)
+        chorale = stream.Score(parts)
+    i = 0
+    while True:
+        print("Enter chord #" + str(i))
+        try:
+            notes = [get_note(msg) for msg in ["Soprano:", "Alto:", "Tenor:", "Bass:"]]
+        except pitch.PitchException as e:
+            print("Returning chorale...")
+            return chorale
+        for part, note in zip(parts, notes):
+            note.offset = i
+            part.append(note)
+        error_checks.check_chorale(chorale, False)
+        i += 1
 
 
 def make_chorale(length):
@@ -18,6 +46,10 @@ def make_chorale(length):
 
     return stream.Score([soprano, alto, tenor, bass])
 
+def get_note(message=None):
+    if message:
+        print(message)
+    return note.Note(input())
 
 def make_part(length):
     part = stream.Part()
