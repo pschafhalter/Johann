1266d6568065e9bfab5ee1fc69ff6fca3fbb0a34
Peter Schafhalter <pschafhalter@berkeley.edu>
2016-02-12 12:58:42 -0800

    More utility functions for making chorales

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 from music21 import note, stream, chord, roman, pitch
-from voice_leading import error_checks
+import voice_leading.error_checks
 
 
 def live_check_chorale(chorale=None):
@@ -26,32 +26,36 @@ def live_check_chorale(chorale=None):
         for part, note in zip(parts, notes):
             note.offset = i
             part.append(note)
-        error_checks.check_chorale(chorale, False)
+
+        check_chorale_errors(chorale)
         i += 1
 
 
-def make_chorale(length):
+
+def write_chorale(length):
     print("Enter soprano notes:")
-    soprano = make_part(length)
+    soprano = write_part(length)
     soprano.id = "soprano"
     print("Enter alto notes:")
-    alto = make_part(length)
+    alto = write_part(length)
     alto.id = "alto"
     print("Enter tenor notes:")
-    tenor = make_part(length)
+    tenor = write_part(length)
     tenor.id = "tenor"
     print("Enter bass notes:")
-    bass = make_part(length)
+    bass = write_part(length)
     bass.id = "bass"
 
     return stream.Score([soprano, alto, tenor, bass])
+
 
 def get_note(message=None):
     if message:
         print(message)
     return note.Note(input())
 
-def make_part(length):
+
+def write_part(length):
     part = stream.Part()
     for i in range(length):
         n = note.Note(input())
@@ -61,7 +65,7 @@ def make_part(length):
 
 
 def check_chorale_errors(chorale):
-    error_checks.check_chorale(chorale)
+    voice_leading.error_checks.check_chorale(chorale, False)
 
 
 def print_chords(chorale):
@@ -70,3 +74,49 @@ def print_chords(chorale):
     for c in chord_list:
         rn = roman.romanNumeralFromChord(c, chorale_key)
         print(rn.figure)
+        
+
+def make_chorale(soprano, alto, tenor, bass):
+    """Given 4 voices, constructs a chorale
+    """
+    soprano.id = "soprano"
+    alto.id = "alto"
+    tenor.id = "tenor"
+    bass.id = "bass"
+
+    return stream.Score([soprano, alto, tenor, bass])
+
+
+def make_chorale_from_strings(soprano, alto, tenor, bass):
+    """Constructs a chorale given string lists of notes
+    """
+    soprano = make_part(map(note.Note, soprano))
+    alto = make_part(map(note.Note, alto))
+    tenor = make_part(map(note.Note, tenor))
+    bass = make_part(map(note.Note, bass))
+
+    return make_chorale(soprano, alto, tenor, bass)
+
+
+def make_part(lst):
+    """Given a list of notes, constructs a voice from them
+    """
+    part = stream.Part()
+    for i, n in enumerate(lst):
+        n.offset = i
+        part.append(n)
+    return part
+
+
+def read_chorale(filename):
+    """Reads a chorale from a given file
+    """
+    f = open(filename)
+
+    s = f.readline().split()
+    a = f.readline().split()
+    t = f.readline().split()
+    b = f.readline().split()
+    
+    return make_chorale_from_strings(s, a, t, b)
+
